fc155e1467a25b01dcc501a2e66304a093d711fa
admin <mohdakram8013@gmail.com>
2021-08-24 17:39:31 +0530

    Stop and start services in foreground service

---
+++ MyServices_forgroundService/app/src/main/java/com/example/myservices_forgroundservice/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myservices_forgroundservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button start, stop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start = (Button) findViewById( R.id.startButton );
+
+        // assigning ID of stopButton
+        // to the object stop
+        stop = (Button) findViewById( R.id.stopButton );
+
+        // declaring listeners for the
+        // buttons to make them respond
+        // correctly according to the process
+        start.setOnClickListener( this );
+        stop.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == start){
+
+            // starting the service
+            startService(new Intent( this, NewService.class ) );
+        }
+
+        // process to be performed
+        // if stop button is clicked
+        else if (view == stop){
+
+            // stopping the service
+            stopService(new Intent( this, NewService.class ) );
+
+        }
+    }
+
+}
